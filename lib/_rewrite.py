99621e285826789982831772072b7fc2c3bc3aad
Waad Nakhleh <waad.nakhleh@intel.com>
2020-11-13 20:43:32 +0200

    Add from import visitor

--- lib/_rewrite.py
+++ lib/_rewrite.py
@@ -7,6 +7,16 @@ class Rewrite(ast.NodeVisitor):
         for name in imports_list:
             file.write(f"import {name.name}\n")
 
+    def visit_ImportFrom(self, node):
+        import_list = node.names
+        file.write(f"from {node.module} import ")
+        for i, name in enumerate(import_list):
+            file.write(f"{name.name}")
+            if i+1 != len(import_list):
+                file.write(", ")
+            else:
+                file.write("\n")
+
 
 def rewrite(file_name: str):
     with open(file_name) as f:
