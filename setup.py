c4ec5a0c0a81e44eae0831c3aac748b7926eb188
Waad Nakhleh <waad.nakhleh@gmail.com>
2021-06-02 12:09:39 +0300

    test setup

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ setuptools.setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    package_dir={"": "src"},
-    packages=setuptools.find_packages(where="src"),
+    package_dir={"": "lib"},
+    packages=setuptools.find_packages(where="lib"),
     python_requires=">=3.8",
 )
