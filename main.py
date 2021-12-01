043fb9e13c93534080aa26d10c71b3133b48b5aa
Waad Nakhleh <waad.nakhleh@gmail.com>
2021-12-01 18:26:48 +0200

    Add special exit code for NoSolutionError

--- main.py
+++ main.py
@@ -1,10 +1,16 @@
 import sys
+import traceback
+
 sys.path.append("lib")
 import _rewrite
 
 
 def main(*argv):
-    _rewrite.rewrite(*argv)
+    try:
+        _rewrite.rewrite(*argv)
+    except:
+        traceback.print_exc()
+        exit(2)
 
 
 if __name__ == "__main__":
